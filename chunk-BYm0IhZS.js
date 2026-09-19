f776078e05512dcc70d9c43129dd0aa6efcaa192
Jazzfer Quijano Iñigo <206848186+Jazzfer-2003@users.noreply.github.com>
2026-09-19 21:23:13 +0800

    Deploy: reviewer v2 cache migration

---
+++ chunk-BYm0IhZS.js
@@ -0,0 +1,1 @@
+import{W as Sl,gt as g,it as bb}from"./main-VO7FANR5.js";import{t as k}from"./chunk-jZNUDmE4.js";var c=class i{sanitizer=g(bb);cache=new Map;transform(t){return this.cache.has(t)||this.cache.set(t,this.sanitizer.bypassSecurityTrustHtml(k(t))),this.cache.get(t)}static ɵfac=function(p){return new(p||i)};static ɵpipe=Sl({name:`ico`,type:i,pure:!0})};export{c as t};
