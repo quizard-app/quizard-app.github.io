1934b7e29b891d423a47f376717378d8dde7fe8c
Jazzfer Quijano Iñigo <206848186+Jazzfer-2003@users.noreply.github.com>
2026-09-22 19:09:12 +0800

    Deploy: AI acronym-casing rules + trueCase safety net

---
+++ chunk-CYVik5NV.js
@@ -0,0 +1,1 @@
+import"./chunk-CCnQ1X3m.js";import"./chunk-Dqyvoa3d.js";import"./chunk-COkV5vgI.js";import{t as b}from"./main-JAATBUVJ.js";export{b as mdTransitionAnimation};
---
+++ chunk-DjDtJtH12.js
@@ -0,0 +1,1 @@
+import"./chunk-CCnQ1X3m.js";import"./chunk-Dqyvoa3d.js";import"./chunk-COkV5vgI.js";import{n as E,r as Y}from"./main-JAATBUVJ.js";export{Y as iosTransitionAnimation};
