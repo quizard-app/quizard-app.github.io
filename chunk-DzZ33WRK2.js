2d872b6489d0a94ad5cce61afd706ec64475e4d8
Jazzfer Quijano Iñigo <206848186+Jazzfer-2003@users.noreply.github.com>
2026-09-24 01:50:51 +0800

    Deploy: faster structured quiz generation

--- chunk-DzZ33WRK2.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-CCnQ1X3m.js";var t=[{path:``,loadComponent:()=>import(`./chunk-BOGI7eWG2.js`).then(o=>o.ExamsPage)},{path:`:id`,loadComponent:()=>import(`./chunk-BOGI7eWG2.js`).then(o=>o.ExamsPage)}];export{t as EXAMS_ROUTES};
