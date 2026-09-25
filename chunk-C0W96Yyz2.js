dd9aaefd903a4c713bbb0895c6f158efe5d1fa5e
Jazzfer Quijano Iñigo <206848186+Jazzfer-2003@users.noreply.github.com>
2026-09-25 15:41:52 +0800

    Deploy: load accounts before resolving the active account in settings

--- chunk-C0W96Yyz2.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-CCnQ1X3m.js";import"./chunk-Dqyvoa3d.js";import"./chunk-1mhF65GX.js";import{t as b}from"./main-MGTMDZMU.js";export{b as mdTransitionAnimation};
---
+++ chunk-DE5t_1Po2.js
@@ -0,0 +1,1 @@
+import"./chunk-CCnQ1X3m.js";import"./chunk-Dqyvoa3d.js";import"./chunk-DPBwbq9L.js";import{n as E,r as Y}from"./main-ZWM54EYN.js";export{Y as iosTransitionAnimation};
