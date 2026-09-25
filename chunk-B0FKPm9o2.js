3effde1cf3a809037452888effa6f3ca1e604ee7
Jazzfer Quijano Iñigo <206848186+Jazzfer-2003@users.noreply.github.com>
2026-09-25 16:33:26 +0800

    Deploy: dedicated restore screen for the sync code (picker opens it directly, no settings detour)

--- chunk-B0FKPm9o2.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-CCnQ1X3m.js";import"./chunk-Dqyvoa3d.js";import"./chunk-Tg3iRx7O.js";import{n as E,r as Y}from"./main-Q3AWNRKZ.js";export{Y as iosTransitionAnimation};
--- chunk-BfgzHpDN2.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-CCnQ1X3m.js";import"./chunk-Dqyvoa3d.js";import"./chunk-Tg3iRx7O.js";import{t as b}from"./main-Q3AWNRKZ.js";export{b as mdTransitionAnimation};
